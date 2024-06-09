b4f3771ae4da4afda61a1e4a530d7d3f78421319
sabinn12 <irakizasabin12@gmail.com>
2024-06-09 21:02:57 +0200

    semi-branch

---
+++ script/script.js
@@ -0,0 +1,22 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const imageInteractions = document.querySelectorAll(".image-interaction");
+
+    imageInteractions.forEach(function (interaction) {
+        const image = interaction.querySelector("img");
+        const overlays = interaction.querySelector(".overlays");
+        const learnMore = interaction.querySelector(".learnmore");
+
+        interaction.addEventListener("mouseover", function () {
+            image.style.filter = "blur(10px)";
+            // overlays.style.transform = "translateY(-20px)";
+            learnMore.style.display = "flex";
+            
+        });
+
+        interaction.addEventListener("mouseout", function () {
+            image.style.filter = "none";
+            overlays.style.transform = "translateY(0)";
+            learnMore.style.display = "none";
+        });
+    });
+});
